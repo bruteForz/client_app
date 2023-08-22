d39f789b8026bc0814c9d13779a160c518df8395
sameera dias <sameera@echonlabs.com>
2023-08-22 15:35:34 +0530

    feat(frontend): :art: created login view and added constant values

---
+++ lib/Constants/elementColors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+const mainFontColor = Color(0xff000000);
+const hintFontColor = Color(0xff6b6b6b);
+const btnColor = Color(0xff29C16E);
+const backgroundColor = Color(0xffFFFFFF);
